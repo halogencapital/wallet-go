4a4eec8873b953e5c3446e5e6ec3cff90c02ddc0
Yahia Sweid <yahia.sweid@gmail.com>
2025-11-18 06:24:10 +0800

    Implement GetClientProfile, GetFund, GetRequestByDuitNowEndToEndID, GetClientAccountAllocationPerformance, GetClientAccountStatement and GetClientAccountRequestConfirmation

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -216,3 +216,230 @@ func (c *Client) ListClientAccounts(ctx context.Context, input *ListClientAccoun
 	}
 	return &output, nil
 }
+
+type Address struct {
+	// permanent, correspondence
+	Type     string  `json:"type,omitempty"`
+	Line1    string  `json:"line1,omitempty"`
+	Line2    *string `json:"line2,omitempty"`
+	City     string  `json:"city,omitempty"`
+	Postcode string  `json:"postcode,omitempty"`
+	State    *string `json:"state,omitempty"`
+	Country  string  `json:"country,omitempty"`
+}
+
+type GetClientProfileInput struct {
+	ClientID string `json:"clientId,omitempty"`
+}
+
+type GetClientProfileOutput struct {
+	Name                  string   `json:"name,omitempty"`
+	Nationality           *string  `json:"nationality,omitempty"`
+	NricNo                *string  `json:"nricNo,omitempty"`
+	PassportNo            *string  `json:"passportNo,omitempty"`
+	Msisdn                *string  `json:"msisdn,omitempty"`
+	Email                 *string  `json:"email,omitempty"`
+	PermanentAddress      *Address `json:"permanentAddress,omitempty"`
+	CorrespondenceAddress *Address `json:"correspondenceAddress,omitempty"`
+
+	// individual, corporate
+	Type                     string  `json:"type,omitempty"`
+	InvestorCategory         string  `json:"investorCategory,omitempty"`
+	CountryOfIncoporation    *string `json:"countryOfIncorporation,omitempty"`
+	AuthorisedPersonName     *string `json:"authorisedPersonName,omitempty"`
+	AuthorisedPersonEmail    *string `json:"authorisedPersonEmail,omitempty"`
+	AuthorisedPersonMsisdn   *string `json:"authorisedPersonMsisdn,omitempty"`
+	AuthorisedPersonOfficeNo *string `json:"authorisedPersonOfficeNo,omitempty"`
+	CompanyRegistrationNo    *string `json:"companyRegistrationNo,omitempty"`
+	OldCompanyRegistrationNo *string `json:"oldCompanyRegistrationNo,omitempty"`
+
+	Ethnicity                *string `json:"ethnicity,omitempty"`
+	DomesticRinggitBorrowing *string `json:"domesticRinggitBorrowing,omitempty"`
+	TaxResidency             *string `json:"taxResidency,omitempty"`
+	CountryTax               *string `json:"countryTax,omitempty"`
+	TaxIdentificationNo      *string `json:"taxIdentificationNo,omitempty"`
+	IncompleteProfile        bool    `json:"incompleteProfile"`
+
+	IsAccountOwner               bool   `json:"isAccountOwner"`
+	CanInvestInUnitTrust         bool   `json:"canInvestInUnitTrust"`
+	CanInvestInDim               bool   `json:"canInvestInDim"`
+	CanUpdateProfile             bool   `json:"canUpdateProfile"`
+	CanSubscribePushNotification bool   `json:"canSubscribePushNotification"`
+	Status                       string `json:"status,omitempty"`
+}
+
+func (c *Client) GetClientProfile(ctx context.Context, input *GetClientProfileInput) (*GetClientProfileOutput, error) {
+	output := GetClientProfileOutput{}
+	err := c.query(ctx, "get_client_profile", input, &output)
+	if err != nil {
+		return nil, err
+	}
+	return &output, nil
+}
+
+type Fund struct {
+	ID                   string                 `json:"id,omitempty"`
+	Type                 string                 `json:"type,omitempty"`
+	Name                 string                 `json:"name,omitempty"`
+	ShortName            string                 `json:"shortName,omitempty"`
+	BaseCurrency         string                 `json:"baseCurrency,omitempty"`
+	Category             string                 `json:"category,omitempty"`
+	Code                 string                 `json:"code,omitempty"`
+	InvestmentObjective  string                 `json:"investmentObjective,omitempty"`
+	InvestorType         string                 `json:"investorType,omitempty"`
+	RiskRating           string                 `json:"riskRating,omitempty"`
+	RiskScore            int                    `json:"riskScore,omitempty"`
+	PrimaryFundManager   string                 `json:"primaryFundManager,omitempty"`
+	SecondaryFundManager string                 `json:"secondaryFundManager,omitempty"`
+	ShariahCompliant     bool                   `json:"shariahCompliant,omitempty"`
+	Status               string                 `json:"status,omitempty"`
+	TagLine              string                 `json:"tagLine,omitempty"`
+	Trustee              string                 `json:"trustee,omitempty"`
+	ImageUrl             string                 `json:"imageUrl,omitempty"`
+	CreatedAt            string                 `json:"createdAt,omitempty"`
+	Classes              []FundClass            `json:"classes,omitempty"`
+	IsOutOfService       bool                   `json:"isOutOfService"`
+	OutOfServiceMessage  string                 `json:"outOfServiceMessage,omitempty"`
+	Metadata             map[string]interface{} `json:"metadata,omitempty"`
+}
+
+type FundClass struct {
+	Sequence                    int                    `json:"sequence,omitempty"`
+	Label                       string                 `json:"label,omitempty"`
+	BaseCurrency                string                 `json:"baseCurrency,omitempty"`
+	ManagementFee               float64                `json:"managementFee,omitempty"`
+	TrusteeFee                  float64                `json:"trusteeFee,omitempty"`
+	CustodianFee                float64                `json:"custodianFee,omitempty"`
+	TransferFee                 float64                `json:"transferFee,omitempty"`
+	TrusteeFeeAnnualMinimum     float64                `json:"trusteeFeeAnnualMinimum,omitempty"`
+	SwitchingFee                float64                `json:"switchingFee,omitempty"`
+	SubscriptionFee             float64                `json:"subscriptionFee,omitempty"`
+	RedemptionFee               float64                `json:"redemptionFee,omitempty"`
+	PerformanceFee              float64                `json:"performanceFee,omitempty"`
+	TaxRate                     float64                `json:"taxRate,omitempty"`
+	MinimumInitialInvestment    float64                `json:"minimumInitialInvestment,omitempty"`
+	MinimumAdditionalInvestment float64                `json:"minimumAdditionalInvestment,omitempty"`
+	MinimumUnitsHeld            float64                `json:"minimumUnitsHeld,omitempty"`
+	MinimumRedemptionAmount     float64                `json:"minimumRedemptionAmount,omitempty"`
+	CanDistribute               bool                   `json:"canDistribute,omitempty"`
+	LaunchPrice                 float64                `json:"launchPrice,omitempty"`
+	HexColor                    string                 `json:"hexColor,omitempty"`
+	CommencementAt              string                 `json:"commencementAt,omitempty"`
+	InitialOfferingPeriodFrom   string                 `json:"initialOfferingPeriodFrom,omitempty"`
+	InitialOfferingPeriodTo     string                 `json:"initialOfferingPeriodTo,omitempty"`
+	CreatedAt                   string                 `json:"createdAt,omitempty"`
+	DistributionFrequency       string                 `json:"distributionFrequency,omitempty"`
+	TagLine                     string                 `json:"tagLine,omitempty"`
+	Metadata                    map[string]interface{} `json:"metadata,omitempty"`
+}
+
+type GetFundInput struct {
+	FundID string `json:"fundId,omitempty"`
+}
+
+type GetFundOutput struct {
+	Fund *Fund `json:"fund,omitempty"`
+}
+
+func (c *Client) GetFund(ctx context.Context, input *GetFundInput) (*GetFundOutput, error) {
+	output := GetFundOutput{}
+	err := c.query(ctx, "get_fund", input, &output)
+	if err != nil {
+		return nil, err
+	}
+	return &output, nil
+}
+
+type GetRequestByDuitNowEndToEndIDInput struct {
+	AccountID  string `json:"accountId,omitempty"`
+	EndToEndID string `json:"endToEndId,omitempty"`
+}
+
+type GetRequestByDuitNowEndToEndIDOutput struct {
+	RequestID string `json:"requestId,omitempty"`
+}
+
+func (c *Client) GetRequestByDuitNowEndToEndID(ctx context.Context, input *GetRequestByDuitNowEndToEndIDInput) (*GetRequestByDuitNowEndToEndIDOutput, error) {
+	output := GetRequestByDuitNowEndToEndIDOutput{}
+	err := c.query(ctx, "get_request_by_duitnow_endToEndId", input, &output)
+	if err != nil {
+		return nil, err
+	}
+	return &output, nil
+}
+
+type AllocationPerformance struct {
+	Date                 string  `json:"date,omitempty"`
+	Units                float64 `json:"units,omitempty"`
+	Asset                string  `json:"asset,omitempty"`
+	NetAssetValuePerUnit float64 `json:"netAssetValuePerUnit,omitempty"`
+	Value                float64 `json:"value,omitempty"`
+	PostFeeAmount        float64 `json:"postFeeAmount,omitempty"`
+}
+
+type GetClientAccountAllocationPerformanceInput struct {
+	AccountID         string `json:"accountId,omitempty"`
+	AllocationID      string `json:"allocationId,omitempty"`
+	Type              string `json:"type,omitempty"`
+	FundClassSequence int    `json:"fundClassSequence,omitempty"`
+	Timeframe         string `json:"timeframe,omitempty"`
+	Interval          string `json:"interval,omitempty"`
+}
+
+type GetClientAccountAllocationPerformanceOutput struct {
+	Performance []AllocationPerformance `json:"performance"`
+}
+
+func (c *Client) GetClientAccountAllocationPerformance(ctx context.Context, input *GetClientAccountAllocationPerformanceInput) (*GetClientAccountAllocationPerformanceOutput, error) {
+	output := GetClientAccountAllocationPerformanceOutput{}
+	err := c.query(ctx, "get_client_account_allocation_performance", input, &output)
+	if err != nil {
+		return nil, err
+	}
+	return &output, nil
+}
+
+type GetClientAccountStatementInput struct {
+	AccountID string `json:"accountId,omitempty"`
+	FromDate  string `json:"fromDate,omitempty"`
+	ToDate    string `json:"toDate,omitempty"`
+	Format    string `json:"format"`
+}
+
+type GetClientAccountStatementOutput struct {
+	FromDate string `json:"fromDate,omitempty"`
+	ToDate   string `json:"toDate,omitempty"`
+	Format   string `json:"format,omitempty"`
+	Filename string `json:"filename,omitempty"`
+	Bytes    []byte `json:"bytes,omitempty"`
+}
+
+func (c *Client) GetClientAccountStatement(ctx context.Context, input *GetClientAccountStatementInput) (*GetClientAccountStatementOutput, error) {
+	output := GetClientAccountStatementOutput{}
+	err := c.query(ctx, "get_client_account_statement", input, &output)
+	if err != nil {
+		return nil, err
+	}
+	return &output, nil
+}
+
+type GetClientAccountRequestConfirmationInput struct {
+	AccountID string `json:"accountId,omitempty"`
+	RequestID string `json:"requestId,omitempty"`
+	Format    string `json:"format,omitempty"`
+}
+
+type GetClientAccountRequestConfirmationOutput struct {
+	Format   string `json:"format,omitempty"`
+	Filename string `json:"filename,omitempty"`
+	Bytes    []byte `json:"bytes,omitempty"`
+}
+
+func (c *Client) GetClientAccountRequestConfirmation(ctx context.Context, input *GetClientAccountRequestConfirmationInput) (*GetClientAccountRequestConfirmationOutput, error) {
+	output := GetClientAccountRequestConfirmationOutput{}
+	err := c.query(ctx, "get_client_account_request_confirmation", input, &output)
+	if err != nil {
+		return nil, err
+	}
+	return &output, nil
+}
