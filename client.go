82f6b5a8de9dc343d758c106a0e53bd2f5b20ed4
Yahia Sweid <yahia.sweid@gmail.com>
2025-11-26 11:16:43 +0800

    Bump version

--- client.go
+++ client.go
@@ -14,7 +14,7 @@ import (
 
 const (
 	endpoint  string = "https://external-api.wallet.halogen.my"
-	version   string = "0.0.6"
+	version   string = "0.0.7"
 	userAgent string = "wallet/" + version + " lang/go"
 )
 
